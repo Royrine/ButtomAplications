af7c54007d94f89973ad744c408762a4be3796f1
logonrm <logonrm@fiap.com.br>
2018-04-06 18:57:56 -0300

    Initial commit

---
+++ app/src/main/java/aplications/buttomaplications/GasCalculate.kt
@@ -0,0 +1,12 @@
+package aplications.buttomaplications
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GasCalculate : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gas_calculate)
+    }
+}
